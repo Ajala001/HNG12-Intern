bfe224f17c52277eafab72a4445cd37d25dd35ec
unknown <ajala.ba2014@gmail.com>
2025-02-06 18:03:49 +0100

    Initial commit

---
+++ HNG12Stage0Api/Controllers/PublicInfoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HNG12Stage0Api.Controllers
+{
+    [ApiController]
+    public class PublicInfoController : Controller
+    {
+
+        [HttpGet("public-info")]
+        public IActionResult GetPublicInfo()
+        {
+            var response = new
+            {
+                email = "ajala.ba2014@gmail.com",
+                current_datetime = DateTime.UtcNow.ToString("o"),
+                github_url = "https://github.com/yourusername/your-repo" // Replace with your GitHub repo URL
+            };
+
+            return Ok(response);
+        }
+    }
+}
