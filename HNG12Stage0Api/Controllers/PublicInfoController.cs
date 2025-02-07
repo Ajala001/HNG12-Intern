f936ce64b033d7d02a26877922dae2d27e09ae8e
unknown <ajala.ba2014@gmail.com>
2025-02-07 15:23:18 +0100

    Update configuration or code changes

--- HNG12Stage0Api/Controllers/PublicInfoController.cs
+++ HNG12Stage0Api/Controllers/PublicInfoController.cs
@@ -3,17 +3,18 @@
 namespace HNG12Stage0Api.Controllers
 {
     [ApiController]
+    [Route("api")]
     public class PublicInfoController : Controller
     {
 
-        [HttpGet("public-info")]
+        [HttpGet("info")]
         public IActionResult GetPublicInfo()
         {
             var response = new
             {
                 email = "ajala.ba2014@gmail.com",
                 current_datetime = DateTime.UtcNow.ToString("o"),
-                github_url = "https://github.com/yourusername/your-repo" // Replace with your GitHub repo URL
+                github_url = "https://github.com/Ajala001/HNG12-Intern.git" 
             };
 
             return Ok(response);
